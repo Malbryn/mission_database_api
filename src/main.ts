c36c50228179da19ff8937a865632e4ecfbea7f2
Dániel Boros <daniel.boros.17@gmail.com>
2022-11-13 16:50:09 +0100

    Enable shutdown hooks in Prisma

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const prismaService = app.get(PrismaService);
+    await prismaService.enableShutdownHooks(app);
 
     app.enableCors();
     app.setGlobalPrefix('api');
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
+            transform: true,
         }),
     );
 
