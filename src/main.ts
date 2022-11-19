f1fa21c6b3ac0132667d573b39e285d12e1e1f08
Dániel Boros <daniel.boros.17@gmail.com>
2022-11-19 18:24:26 +0100

    Update upload limit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -17,7 +18,10 @@ async function bootstrap() {
             transform: true,
         }),
     );
+    app.use(json({ limit: '10mb' }));
+    app.use(urlencoded({ extended: true, limit: '10mb' }));
 
     await app.listen(3000);
 }
+
 bootstrap();
