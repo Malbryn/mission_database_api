31fd842f8c4183fbab74c3e8022fcc79b5afe97f
Dániel Boros <daniel.boros.17@gmail.com>
2022-11-24 21:38:37 +0100

    Exclude password from get user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
+import { exclude } from '../helpers/exclude';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
     @Get('me')
     getMe(@GetUser() user: User) {
-        return user;
+        return { ...exclude(user, ['password']) };
     }
 
     @Patch()
